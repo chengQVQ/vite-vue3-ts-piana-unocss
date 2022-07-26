82d33feb6e76c31180eebe5bbc311c19b794a12a
cheng <64277988+chengQVQ@users.noreply.github.com>
2022-07-26 14:46:37 +0800

    default

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import "uno.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from 'pinia' 
+import '@unocss/reset/tailwind.css' //重置样式 如果ui组件出现没颜色的问题 可能是这个导致的
 const pinia = createPinia()
 createApp(App).use(pinia).mount('#app')
