06cd90ea72723f3a2f341bd3c32d377866719a6f
cheng <64277988+chengQVQ@users.noreply.github.com>
2022-08-04 18:02:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <a-tabs tab-position="left">
-    <a-tab-pane tab="杂" key="1">
+    <a-tab-pane tab="unocss" key="1">
       <test />
     </a-tab-pane>
     <a-tab-pane tab="pinia" key="2"><pinia/></a-tab-pane>
-    <a-tab-pane tab="Tab 3" key="3">Content of Tab Pane 3</a-tab-pane>
   </a-tabs>
 </template>
 
