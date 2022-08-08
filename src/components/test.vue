012319d5e4146685f049910c551e45bb673ba7e3
cheng <64277988+chengQVQ@users.noreply.github.com>
2022-08-08 10:30:55 +0800

    computed

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,18 +1,33 @@
 <template>
-  <a-button type="primary">hahaha</a-button>
+  <a-button type="primary" @click="add">hahaha</a-button>
   <hello-world />
   <img src="@/assets/1.jpg" class="w-10px" alt="">
 
   <div mt-13px class="h-[calc(100px-10px)]">123</div>
   <div mt-13px flex-c>456</div>
+  <div>{{n}}</div>
 </template>
 
 <script>
-export default {
-
-}
+export default defineComponent({
+  setup() {
+    const obj = reactive({
+      a: 1,
+      b: 2,
+    });
+    const n = computed(() => {
+      return obj.a + obj.b;
+    });
+    const add = () => {
+      obj.a += 2;
+    };
+    return {
+      n,
+      add,
+    };
+  },
+});
 </script>
 
 <style>
-
 </style>
