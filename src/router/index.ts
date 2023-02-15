39dd246593cb49f5dc421c89120fda02ed1c602d
cheng <64277988+chengQVQ@users.noreply.github.com>
2023-02-15 11:14:23 +0800

    删除多余的

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import layout from "@/layout/index.vue";
-import login from "@/views/login/index.vue";
 import home from "@/views/home/index.vue";
 let routes = [
   {
     path: "/",
-    name: 'layout',
-    component: layout,
-  },
-  {
-    path: "/home",
     name: "home",
     component: home,
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: login,
-  },
+  }
 ];
 
 const router = createRouter({
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <a-tabs tab-position="left">
-    <a-tab-pane tab="unocss" key="1">
-      <test />
-    </a-tab-pane>
-    <a-tab-pane tab="pinia" key="2">
-      <pinia />
-    </a-tab-pane>
-  </a-tabs>
+  <div></div>
 </template>
 
 <script>
+export default defineComponent({
+
+})
 </script>
 
-<style>
+<style lang="less" scoped>
+
 </style>
